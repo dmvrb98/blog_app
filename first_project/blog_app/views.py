5812bb34a4925cd73ca9e00a9880f3249f4ed45d
dmvrb98 <dmytro.vorobiov98@gmail.com>
2021-04-12 15:24:14 +0200

    some  changes

--- first_project/blog_app/views.py
+++ first_project/blog_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
 	return render(request, 'blog_app/index.html')
@@ -14,5 +15,32 @@ def topic(request, topic_id):
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'blog_app/topic.html', context)
+
+def new_topic(request):
+	if request.method != 'POST':
+		form = TopicForm()
+	else:
+		form = TopicForm(data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('blog_app:topics')
+
+	context = {'form': form}
+	return render(request, 'blog_app/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+	topic = Topic.objects.get(id=topic_id)
+	if request.method !='POST':
+		form = EntryForm()
+	else:
+		form = EntryForm(data=request.POST)
+		if form.is_valid():
+			new_entry = form.save(commit=False)
+			new_entry.topic = topic
+			new_entry.save()
+			return redirect('blog_app:topic', topic_id=topic_id)
+	context = {'topic': topic, 'form': form}
+	return render(request, 'blog_app/new_entry.html', context)
 
 # Create your views here.
