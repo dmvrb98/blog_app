d460071edc71effa067ca64e7ac037b260037dbd
Dmytro Vorobiov <dmvrb98@MacBook-Air-Dmytro.local>
2021-04-22 16:54:41 +0200

    added comments

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -31,8 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #my_apps
     'blog_app',
     'users',
+    #other_apps
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'users:login'
