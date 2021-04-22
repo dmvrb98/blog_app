76f919d92797ffbacbd1bf40cbec8c70d38d2cb2
Dmytro Vorobiov <dmvrb98@MacBook-Air-Dmytro.local>
2021-04-22 17:08:29 +0200

    added comments

--- first_project/blog_app/views.py
+++ first_project/blog_app/views.py
@@ -5,23 +5,31 @@ from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect, Http404
 
 def index(request):
+	'''this one is rendering home page, and this is the only one page that
+	visitor can see without registration'''
 	return render(request, 'blog_app/index.html')
 
 @login_required
 def topics(request):
+	'''this one is showing all topics ordered by date'''
 	topics = Topic.objects.filter(owner=request.user).order_by('date_added')
 	context = {'topics': topics}
 	return render(request, 'blog_app/topics.html', context)
+
 @login_required
 def topic(request, topic_id):
+	'''this one shows all entries bellowing to a chosen topic starting
+	from the latest one'''
 	topic = Topic.objects.get(id=topic_id)
 	if topic.owner != request.user:
 		raise Http404
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'blog_app/topic.html', context)
+
 @login_required
 def new_topic(request):
+	'''this one is creating new topic and binding user with topic'''
 	if request.method != 'POST':
 		form = TopicForm()
 	else:
@@ -37,6 +45,7 @@ def new_topic(request):
 
 @login_required
 def new_entry(request, topic_id):
+	'''this one is creating new entry in a chosen topic'''
 	topic = Topic.objects.get(id=topic_id)
 	if request.method !='POST':
 		form = EntryForm()
@@ -52,6 +61,7 @@ def new_entry(request, topic_id):
 
 @login_required
 def edit_entry(request, entry_id):
+	'''this one allows editors to make changes in their entries'''
 	entry = Entry.objects.get(id=entry_id)
 	topic = entry.topic
 	if topic.owner != request.user:
