890c0bb60edf3881d7db8c78d5af84226807b500
Dmytro Vorobiov <dmvrb98@MacBook-Air-Dmytro.local>
2021-04-22 16:56:21 +0200

    added comments

--- first_project/blog_app/views.py
+++ first_project/blog_app/views.py
@@ -1,34 +1,41 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from django.contrib.auth.decorators import login_required
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.http import HttpResponseRedirect, Http404
 
 def index(request):
 	return render(request, 'blog_app/index.html')
 
+@login_required
 def topics(request):
-	topics = Topic.objects.order_by('date_added')
+	topics = Topic.objects.filter(owner=request.user).order_by('date_added')
 	context = {'topics': topics}
 	return render(request, 'blog_app/topics.html', context)
-
+@login_required
 def topic(request, topic_id):
 	topic = Topic.objects.get(id=topic_id)
+	if topic.owner != request.user:
+		raise Http404
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'blog_app/topic.html', context)
-
+@login_required
 def new_topic(request):
 	if request.method != 'POST':
 		form = TopicForm()
 	else:
 		form = TopicForm(data=request.POST)
 		if form.is_valid():
-			form.save()
+			new_topic = form.save(commit=False)
+			new_topic.owner = request.user
+			new_topic.save()
 			return redirect('blog_app:topics')
 
 	context = {'form': form}
 	return render(request, 'blog_app/new_topic.html', context)
 
-
+@login_required
 def new_entry(request, topic_id):
 	topic = Topic.objects.get(id=topic_id)
 	if request.method !='POST':
@@ -42,5 +49,24 @@ def new_entry(request, topic_id):
 			return redirect('blog_app:topic', topic_id=topic_id)
 	context = {'topic': topic, 'form': form}
 	return render(request, 'blog_app/new_entry.html', context)
+
+@login_required
+def edit_entry(request, entry_id):
+	entry = Entry.objects.get(id=entry_id)
+	topic = entry.topic
+	if topic.owner != request.user:
+		raise Http404
+
+	if request.method !='POST':
+		form=EntryForm(instance=entry)
+	else:
+		form = EntryForm(instance=entry, data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('blog_app:topic', topic_id=topic.id)
+
+	context = {'entry': entry, 'topic': topic, 'form': form}
+	return render(request, 'blog_app/edit_entry.html', context)
+
 
 # Create your views here.
